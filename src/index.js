3b9342e1d64c75b1fead44b0aaf706c02bcb44ea
shawasty <shawasty@gmail.com>
2022-04-12 08:40:52 -0400

    added some styles

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+// import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import birdApp from './store/birds/birds';
 
-const store = createStore(() => ({
-  birds: [
-    {
-      name: 'robin',
-      views: 1
-    }
-  ]
-}));
+const store = createStore(birdApp) ;
+// => {
+  // birds: [
+  //   {
+  //     name: 'robin',
+  //     views: 1
+  //   }
+  // ]
+// }));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
